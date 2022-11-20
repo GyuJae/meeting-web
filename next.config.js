fa8e174830257293b7c7c8706fc54430438115ce
정규재 <wjdrbwo1206@naver.com>
2022-11-20 15:23:29 +0900

    initial nextjs style and routes setting

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
+const path = require('path');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  pageExtensions: ['page.tsx', 'page.ts', 'page.jsx', 'page.js'],
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles'), path.join(__dirname, 'pages'), path.join(__dirname, 'components')],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
